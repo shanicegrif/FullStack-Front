5bd79352e0b52826251f953b092605b27e9245e6
dreamseekerfromn <sy@Sungs-Air.fios-router.home>
2024-01-25 21:14:22 -0500

    updates fetch.js; 7/8 are done

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -12,6 +12,25 @@ export async function getMeetingRooms(){
     return await axios.get(`${URL}/meetingRooms`);
 };
 
-export async function getOneRoom(id){
+export async function getOneMeetingRoom(id){
     return await axios.get(`${URL}/meetingRooms/${id}`);
+};
+
+export async function getBookings(){
+    return await axios.get(`${URL}/bookings`);
+};
+
+export async function getOneBooking(id){
+    return await axios.get(`${URL}/bookings/${id}`);
+};
+
+export async function deleteBooking(id){
+    return await axios.delete(`${URL}/bookings/${id}`);
+};
+
+export async function postBooking(body){
+    return await axios.post(`${URL}/bookings`, body, {headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+    }});
 };
