05b9b62e376ada9b8175c7cfa983a5c772e0ad53
dreamseekerfromn <sy@Sungs-Air.fios-router.home>
2024-01-26 00:49:53 -0500

    minor update for route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FourOFour from './pages/FourOFour';
 import './App.css';
+import Bookings from './pages/Bookings';
+import NewRoom from './pages/NewRoom';
 
 function App() {
 
@@ -11,7 +13,11 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/bookings" element={<Bookings />} />
+            {/** <Route path="/bookings/:id element={} />" */}
+            <Route path="/newroom" element={<NewRoom />} />
             <Route path="*" element={<FourOFour />} />
+            
           </Routes>
         </main>
       </Router>
---
+++ src/pages/Bookings.jsx
@@ -0,0 +1,10 @@
+/**
+ * Bookings()
+ * ================================
+ * renders the booking list tab.
+ * 
+ * @returns {ReactComponentElement}
+ */
+export default function Bookings(){
+
+}
