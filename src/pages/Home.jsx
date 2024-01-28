be47ecda6580651cf1883ccbe7ddb2e60ed4e5c1
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2024-01-28 04:24:11 -0500

    worked on NewRoom and homepg components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,13 @@
-/**
- * Home()
- * ================================
- * renders the meeting room list tab.
- * 
- * @returns {ReactComponentElement}
- */
-function Home() {
-  return (
-    <div className="home-container">
+import {useState} from 'react';
+import FindAvailableRoom from '../components/FindAvailableRoom';
 
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to The NIfel Towers</h1>
+      <FindAvailableRoom />
     </div>
   );
-}
+};
 
-export default Home;
+export default HomePage;
---
+++ src/pages/NewRoom.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NewRoomForm from '../components/NewRoomForm';
+
+const NewRoomPage = () => {
+  return (
+    <div>
+      <h1>Create a New Room</h1>
+      <NewRoomForm />
+    </div>
+  );
+};
+
+export default NewRoomPage;
