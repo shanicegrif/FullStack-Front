f8afdbcf461aab8894e7372964d8c4e22fc08524
Genesis Lara <genesislara@pursuit.org>
2024-01-28 09:36:31 -0500

    saves new files

--- src/pages/SingleBooking.jsx
+++ src/pages/SingleBooking.jsx
@@ -2,5 +2,15 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const SingleBooking = () => {
+
+
+ 
   
+
+  return (
+    <div>
+      
+    </div>
+
+  )
 }
