e1449a0ff642041e0e8fc773c98bd2758f1f4335
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2024-01-28 17:11:05 -0500

    event spaces which will show the list of events

---
+++ src/components/NewEventSpace.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { getMeetingRooms } from '../api';
+
+const NewEventSpace = () => {
+    const [eventSpaces, setEventSpaces] = useState([]);
+
+useEffect(() => {
+    const fetchNewEventSpaces = async () => {
+      try {
+        const spaces = await getMeetingRooms();
+        setEventSpaces(spaces);
+      } catch (error) {
+        console.error('Error fetching event spaces:', error);
+      }
+    };
+
+    fetchNewEventSpaces();
+  }, []);
+
+  return (
+    <div className="container event-space">
+      <h2>Event Spaces</h2>
+      <ul className="event-space-list">
+        {eventSpaces.map(space => (
+          <li key={space.id} className="-event-space-list-item">
+            <Link to={`/event-spaces/${space.id}`}>
+              {space.name} - {space.location}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+
+export default NewEventSpace;
